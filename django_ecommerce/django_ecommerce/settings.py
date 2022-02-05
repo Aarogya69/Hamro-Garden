ef0936b9dc5c6859afc7b83a02dcdba3bb3cb4fe
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2022-02-05 14:39:14 +0545

    Stripe working

--- django_ecommerce/django_ecommerce/settings.py
+++ django_ecommerce/django_ecommerce/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STRIPE_SECRET_KEY = 'sk_test_51HIHiuKBJV2qeWbD4IBpAODack7r7r9LJ0Y65zSFx7jUUwgy2nfKEgQGvorv1p2xp7tgMsJ5N9EW7K1lBdPnFnyK00kdrS27cj'
+STRIPE_SECRET_KEY = 'sk_test_51KP3FqEtGLzL5qASeuMfb1rsMDAHmhdPgXWUFJ5Xo6wyyEtSSv4ws4B7ufZY9LHCtyzfMYO9uRkxgE89ajanjLEy0040hd3xJW'
 
 # Application definition
 
