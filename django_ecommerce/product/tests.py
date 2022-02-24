4fe051677b6b8a346a9f4ddd460ab18358e8a47f
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2022-02-24 13:09:17 +0545

    unit testing

--- django_ecommerce/product/tests.py
+++ django_ecommerce/product/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
+from product.models import Category
+
 # Create your tests here.
+
+class BasicTest(TestCase):
+
+    def test_fields(self):
+
+        category = Category()
+
+        category.name= "Plants"
+
+        category.slug= "plants"
+
+        category.save()
+
+       
+
+        check = Category.objects.get(id=1)
+
+        self.assertEqual(check, category)
+
+
